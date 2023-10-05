a22c2cbd5a3ea6cc8effe4da09d1bad312c8c89e
Max Johnson <114068285+MaxJ02@users.noreply.github.com>
2023-10-05 11:49:57 +0200

    Update send.py

--- send.py
+++ send.py
@@ -4,8 +4,8 @@ import paho.mqtt.client as mqtt
 # Funktion för att kryptera meddelanden med rot-n
 def encrypt(message, n):
     alphabet = "abcdefghijklmnopqrstuvwxyzåäö"
-    encrypted_message = ""
-
+    encrypted_message = ''
+    
     for char in message:
         if char.lower() in alphabet:
             is_upper = char.isupper()
@@ -19,7 +19,6 @@ def encrypt(message, n):
             encrypted_message += char
     return encrypted_message
 
-
 # Anslut till MQTT-brokern
 client = mqtt.Client()
 client.connect("broker.hivemq.com", port=1883, keepalive=60)
@@ -29,18 +28,18 @@ client.loop_start()
 
 # Läs in användarnamn från användaren
 username = input("Ange användarnamn: ")
-# Läs in förskjutning från användaren
-shift = int(input("Ange en krypterings-förskjutning mellan 0 - 28: "))
 
 try:
     while True:
+        # Läs in förskjutning från användaren
+        shift = int(input("Ange en krypterings-förskjutning mellan 0 - 28: "))
         # Läs in text från användaren
         message = input("Ange meddelande (eller tryck Enter för att avsluta): ")
-
+        
         # Avsluta om användaren inte skriver något
         if not message:
             break
-
+        
         # Skicka meddelandet till MQTT-brokern på angiven topic
 
         encrypted_message = encrypt(message, shift)
@@ -51,3 +50,5 @@ except KeyboardInterrupt:
 # Stoppa MQTT-loopen och koppla från
 client.loop_stop()
 client.disconnect()
+
+#client.publish("ela/superchat", payload=message_with_username, qos=1)
